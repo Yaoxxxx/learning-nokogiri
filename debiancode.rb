62ec57cdd57df2406c5868be732a7874de004a0f
Vagrant Default User <vagrant@jessie.raw>
2016-11-14 08:25:36 +0000

    完成しました

--- debiancode.rb
+++ debiancode.rb
@@ -7,6 +7,13 @@ require 'nokogiri'
 url = 'https://wiki.debian.org/DebianReleases'
 html = open(url).read
 
-puts html
-# doc = Nokogiri::HTML.parse(html)
-# pp doc
+# puts html
+
+doc = Nokogiri::HTML.parse(html)
+doc.xpath('//table').first.xpath('.//tr').each do |e|
+  e.xpath('.//td').each do |t|
+    print t.inner_text.ljust(25,' ')
+  end
+  puts
+end
+
